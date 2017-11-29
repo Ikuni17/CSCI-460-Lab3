26380d80fc828c17a84d742c600afa675561ff85
BradWhite <white.brad17@gmail.com>
2017-11-29 14:48:35 -0700

    Changed to random sampling

--- white_bradley - 3.py
+++ white_bradley - 3.py
@@ -12,14 +12,17 @@ Outputs: Writes to console
 import random
 
 # Creates n random jobs in the form (arrival time, job type) and returns them as a list of tuples
-def random_jobs(n):
+def random_jobs(n, max_arrival):
     jobs = []
+    # Use sampling so we cannot have two jobs arrive at the same time
+    assert n < max_arrival
+    arrivals = random.sample(range(max_arrival), k=n)
 
     for i in range(n):
         # Get a random job number
         job = random.randint(1, 3)
         # Get a random arrival time
-        arrival = random.randint(1, 50)
+        arrival = arrivals[i]
         # Add the new tuple to the list of jobs
         jobs.append((arrival, job))
 
@@ -34,8 +37,8 @@ def main():
     print('Starting job sequence: {0}'.format(jobs))
 
     # Random job sequence
-    rand_jobs = random_jobs(10)
-    print(rand_jobs)
+    rand_jobs = random_jobs(10, 50)
+    print('Starting job sequence: {0}'.format(rand_jobs))
 
 if __name__ == '__main__':
     main()
